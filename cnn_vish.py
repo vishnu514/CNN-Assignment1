431d4c62a3305fc89675da4fbbb0e2b533973f15
vishnu514 <56917443+vishnu514@users.noreply.github.com>
2020-04-04 23:11:03 +0530

    Add files via upload

---
+++ cnn_vish.py
@@ -0,0 +1,104 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+#import keras
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+
+classifier.add(Dense(units = 3, activation = 'softmax'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('C:/Users/Vishnu/Desktop/ineuron/CNN/Vishdemo',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+
+test_set = test_datagen.flow_from_directory('C:/Users/Vishnu/Desktop/ineuron/CNN/Vishdemo',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 1000,
+                         epochs = 1,
+                         validation_data = test_set,    
+                         validation_steps = 2000)
+
+# Part 3 - Making new predictions
+
+
+
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('C:/Users/Vishnu/Desktop/ineuron/CNN/balu.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+
+for i in result:
+    print('final',i)
+print('test1:',result[0,2])
+print('test1:',result[0,1])
+print('test1:',result[0,0])
+
+if result[0,2] == 1:
+    prediction = 'Aru'
+    print(prediction)
+elif result[0,1] == 1:
+    prediction = 'Balu'
+    print(prediction)
+else:
+    prediction = 'vishnu'
+    print(prediction)
+    
+    
